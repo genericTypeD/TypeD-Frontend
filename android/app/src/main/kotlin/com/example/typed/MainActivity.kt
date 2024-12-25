48a240fc290a569a4d1ab3a02451192db556ac49
hye-inA <hye.ina0130@gmail.com>
2024-12-26 02:45:39 +0900

    flutter project create

---
+++ android/app/src/main/kotlin/com/example/typed/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.typed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
