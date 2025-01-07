5972bf4797b0a3df73c36281fdffe9a7dbfe1ccb
hye-inA <hye.ina0130@gmail.com>
2025-01-07 13:06:50 +0900

    feat : common app string 추가

---
+++ lib/common/const/app_strings.dart
@@ -0,0 +1,32 @@
+class AppStrings {
+  const AppStrings._();
+
+  static const String PageNotFound = '페이지를 찾을 수 없습니다';
+
+  static const String register = '회원가입';
+  static const String login = '로그인';
+  static const String signInToYourAccount = '로그인 해주세요';
+
+  static const String forgetPassword = '비밀번호를 잊어버리셨나요?';
+  static const String findPassword = '비밀번호 찾기';
+
+  static const String doNotHaveAnAccount = "계정이 없으신가요?";
+  static const String createYourAccount = '회원가입 하러가기';
+
+  static const String orLoginWith = '다른 계정으로 로그인';
+  static const String kakaotalkLogin = '카카오 계정으로 계속하기';
+  static const String appleLogin = '애플 계정으로 계속하기';
+  static const String googleLogin = '구글 계정으로 계속하기';
+
+  static const String nickname = '닉네임';
+  static const String pleaseEnterNickname = '닉네임을 입력해주세요';
+  static const String invalidNickname = '유효하지 않는 닉네임';
+
+  static const String email = '이메일';
+  static const String pleaseEnterEmailAddress = '이메일을 입력해주세요.';
+  static const String invalidEmailAddress = '유효하지 않는 이메일입니다.';
+
+  static const String password = '비밀번호';
+  static const String pleaseEnterPassword = '비밀번호를 입력해주세요.';
+  static const String invalidPassword = '유효하지 않는 비밀번호입니다.';
+}
