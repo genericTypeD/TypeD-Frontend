16aec423a078d4c81aa9a5261d13e2bec05821c8
hye-inA <hye.ina0130@gmail.com>
2025-01-13 13:36:35 +0900

    refactor : defaultLayout, main 파일 리팩토링

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:typed/common/layout/default_layout.dart';
-import 'package:typed/feed/screen/feed_list_page.dart';
-import 'package:typed/menu/screen/my_account.dart';
-import 'package:typed/review/screen/reading_empty_page.dart';
-import 'package:typed/sentence/screen/empty_page.dart';
-import 'package:typed/type/screen/my_type.dart';
+import 'package:provider/provider.dart';
+import 'package:typed/common/screen/home_tab.dart';
+import 'package:typed/sentence/provider/sentence_provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SentenceProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,48 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DefaultLayoutWithNavigation(), // DefaultLayout을 루트로 설정
-    );
-  }
-}
-
-class DefaultLayoutWithNavigation extends StatefulWidget {
-  @override
-  State<DefaultLayoutWithNavigation> createState() =>
-      _DefaultLayoutWithNavigationState();
-}
-
-class _DefaultLayoutWithNavigationState
-    extends State<DefaultLayoutWithNavigation> {
-  int _currentIndex = 0;
-
-  final List<Widget> _pages = [
-    MyType(),
-    ReadingEmptyPage(),
-    EmptyPage(),
-    FeedListPage(),
-    MyAccount(),
-  ];
-
-  final List<String> _titles = [
-    'My Type',
-    '서평 메모',
-    '문장 수집',
-    '취향 탐색',
-    '나의 계정',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultLayout(
-      title: _titles[_currentIndex], // 현재 페이지 제목
-      currentIndex: _currentIndex, // 현재 선택된 인덱스
-      child: _pages[_currentIndex], // 현재 페이지를 child로 전달
-      onItemTapped: (index) {
-        setState(() {
-          _currentIndex = index; // 탭 전환 처리
-        });
-      },
+      home: HomeTab(),
     );
   }
 }
