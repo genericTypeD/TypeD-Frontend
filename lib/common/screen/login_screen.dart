e3b55a3dd0274532a95db1261804298c5db21b03
hye-inA <hye.ina0130@gmail.com>
2025-02-05 14:57:57 +0900

    feat(screen) : MyMenu UI 구현

---
+++ lib/common/screen/login_screen.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:typed/common/component/custom_text_form_field.dart';
+import 'package:typed/common/layout/default_layout.dart';
+
+import '../const/app_strings.dart';
+import '../const/app_themes.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      child: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 100),
+                // 상단 타이틀
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '나의 컬렉션 Mate',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'TypeD',
+                      style: AppTheme.heading1,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 48),
+                CustomTextFormField(
+                  onChanged: (String value) {
+                    email = value;
+                  },
+                  hintText: '이메일을 입력해주세요',
+                  labelText: AppStrings.email,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 16),
+                CustomTextFormField(
+                  onChanged: (String value) {
+                    password = value;
+                  },
+                  hintText: '비밀번호를 입력해주세요',
+                  labelText: AppStrings.password,
+                  obscureText: true,
+                  textInputAction: TextInputAction.done,
+                  keyboardType: TextInputType.visiblePassword,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        '비밀번호를 잊어버리셨나요?',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '비밀번호찾기',
+                      style: TextStyle(
+                        color: Colors.grey[800],
+                        fontSize: 12,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[200],
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    '로그인',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Center(
+                  child: Text(
+                    '다른 계정으로 로그인',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFFE500), // 카카오 노란색
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.chat_bubble,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        '카카오 계정으로 계속하기',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Center(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text.rich(
+                      TextSpan(
+                        text: '계정이 없으신가요? ',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 12,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: '회원가입 하러가기',
+                            style: TextStyle(
+                              color: Colors.black,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/menu/screen/my_menu.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:typed/common/const/app_colors.dart';
+import 'package:typed/common/screen/login_screen.dart';
+
+class MyMenu extends StatefulWidget {
+  const MyMenu({super.key});
+
+  @override
+  State<MyMenu> createState() => _MyMenuState();
+}
+
+class _MyMenuState extends State<MyMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.backgroundPrimary,
+      child: Column(
+        children: [
+          _buildProfileCard(),
+          _buildMenuList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileCard() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 6,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          const CircleAvatar(
+            radius: 40,
+            backgroundColor: Color(0xFFE6E6E6),
+            child: Icon(
+              Icons.person_outline,
+              size: 40,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 12),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const LoginScreen(), // 로그인 스크린 위젯
+                ),
+              );
+            },
+            child: Center(
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    '로그인이 필요합니다',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(Icons.chevron_right)
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          // 통계 정보
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildStatItem('8', '내가 읽은 책'),
+              _buildDivider(),
+              _buildStatItem('-', '북마크'),
+              _buildDivider(),
+              _buildStatItem('80%', '문장 수집률'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDivider() {
+    return Container(
+      height: 40,
+      width: 1,
+      color: Colors.grey[300],
+    );
+  }
+
+  Widget _buildStatItem(String value, String label) {
+    return Column(
+      children: [
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.grey[600],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMenuList() {
+    final menuItems = [
+      {'title': '로그인 정보', 'icon': Icons.person_outline},
+      {'title': '고객센터', 'icon': Icons.headset_mic_outlined},
+      {'title': '약관', 'icon': Icons.description_outlined},
+    ];
+
+    return Container(
+      color: AppColors.whitePrimary,
+      child: ListView.separated(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: menuItems.length,
+        separatorBuilder: (context, index) => Divider(
+          height: 1,
+          color: Colors.grey[300],
+        ),
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(
+              menuItems[index]['icon'] as IconData,
+              color: Colors.black,
+            ),
+            title: Text(menuItems[index]['title'] as String),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {},
+          );
+        },
+      ),
+    );
+  }
+}
