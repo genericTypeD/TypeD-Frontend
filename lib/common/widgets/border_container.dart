a618e6313c9116978f6d13a226d41e70e377f8b5
H <instantseaside@gmail.com>
2025-02-12 02:14:49 +0900

    feat: CustomAppBar용 컨테이너 위젯 구현

---
+++ lib/common/widgets/border_container.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+enum ContainerBorderType {
+  left,
+  right;
+
+  BorderSide get borderSide {
+    return BorderSide(
+      color: Colors.black,
+      width: 0.3,
+    );
+  }
+}
+
+class BorderContainer extends StatelessWidget {
+  final ContainerBorderType type;
+
+  const BorderContainer({
+    required this.type,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 16.0,
+      decoration: BoxDecoration(
+        border: Border(
+          left: type == ContainerBorderType.right
+              ? type.borderSide
+              : BorderSide.none,
+          right: type == ContainerBorderType.left
+              ? type.borderSide
+              : BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
