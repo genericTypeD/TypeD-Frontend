1c2f60672621abfdc92cd8d804b1bb77db5d5e14
H <instantseaside@gmail.com>
2025-02-12 03:14:02 +0900

    refactor: 앱바 스타일 상수 AppBarStyle로 분리

---
+++ lib/common/const/app_bar_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppBarStyle {
+  static const double headerHeight = 50.0;
+  static const Color backgroundColor = Color(0xffF3F3F2);
+  static const BorderSide borderStyle = BorderSide(
+    color: Colors.black,
+    width: 0.3,
+  );
+}
