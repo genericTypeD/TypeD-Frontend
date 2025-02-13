577b52765f87c25d7ed4fed7fe7b5a84790baba2
H <instantseaside@gmail.com>
2025-02-14 04:35:36 +0900

    feat: 갤러리 이미지 선택 기능 추가

--- lib/type/screen/my_image_screen.dart
+++ lib/type/screen/my_image_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:typed/common/layout/default_layout.dart';
 
 class MyMovieScreen extends ConsumerStatefulWidget {
@@ -16,6 +17,37 @@ class MyMovieScreen extends ConsumerStatefulWidget {
 }
 
 class _MyMovieScreenState extends ConsumerState<MyMovieScreen> {
+  final ImagePicker _picker = ImagePicker();
+
+  XFile? _selectedImageFile;
+  final List<XFile> _currentImageFiles = [];
+
+  Future<void> _pickImage() async {
+    if (await Permission.storage.request().isGranted) {
+      try {
+        final XFile? image =
+            await _picker.pickImage(source: ImageSource.gallery);
+        if (image != null) {
+          setState(() {
+            _selectedImageFile = image;
+            _currentImageFiles.insert(0, image);
+          });
+        }
+      } catch (e) {
+        debugPrint('[Picking Image Error] $e');
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.initialImage != null) {
+      _selectedImageFile = widget.initialImage!;
+      _currentImageFiles.add(widget.initialImage!);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const DefaultLayout(child: Column());
