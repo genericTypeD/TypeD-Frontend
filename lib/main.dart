0f854867d408771d3fe2b121fff7d7c2fe9b58e5
Soojin Son <greennesson@gmail.com>
2025-02-13 21:57:02 +0900

    refactor: Custom appBar merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:typed/common/provider/app_routes.dart'; // AppRoutes 임포트
+import 'package:typed/common/provider/app_routes.dart';
 import 'package:typed/common/screen/splash.dart';
 import 'package:typed/sentence/provider/sentence_provider.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // ✅ 앱 실행 시 한 번만 설정
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarBrightness: Brightness.light,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
-
   runApp(
     MultiProvider(
       providers: [
