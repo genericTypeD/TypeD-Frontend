d72be644303f5aa159aba78d5fbd09b9d2ccc73e
H <instantseaside@gmail.com>
2025-02-19 00:06:15 +0900

    chore: iOS 빌드 설정 충돌 해결

--- lib/sentence/screen/sentence_empty.dart
+++ lib/sentence/screen/sentence_empty.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:typed/common/const/app_colors.dart';
 import 'package:typed/common/const/app_themes.dart';
 import 'package:typed/common/layout/default_layout.dart';
-import 'package:typed/common/widgets/custom_app_bar.dart';
+// import 'package:typed/common/widgets/custom_app_bar.dart';
 
 class SentenceEmpty extends StatelessWidget {
   const SentenceEmpty({super.key});
@@ -10,32 +10,32 @@ class SentenceEmpty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      appBar: CustomAppBar(
-        bottomLeftWidget: GestureDetector(
-          onTap: () {
-            debugPrint('클릭됨!');
-          },
-          child: Text(
-            "텍스트A",
-            style: AppTheme.title3,
-          ),
-        ),
-        bottomRightWidget: GestureDetector(
-          onTap: () {
-            debugPrint('클릭됨!');
-          },
-          child: Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 8),
-              child: Text(
-                "글쓰기",
-                style: AppTheme.title3,
-              ),
-            ),
-          ),
-        ),
-      ),
+      // appBar: CustomAppBar(
+      //   bottomLeftWidget: GestureDetector(
+      //     onTap: () {
+      //       debugPrint('클릭됨!');
+      //     },
+      //     child: Text(
+      //       "텍스트A",
+      //       style: AppTheme.title3,
+      //     ),
+      //   ),
+      //   bottomRightWidget: GestureDetector(
+      //     onTap: () {
+      //       debugPrint('클릭됨!');
+      //     },
+      //     child: Align(
+      //       alignment: Alignment.centerRight,
+      //       child: Padding(
+      //         padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 8),
+      //         child: Text(
+      //           "글쓰기",
+      //           style: AppTheme.title3,
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
       child: Container(
         color: AppColors.backgroundSecondary,
         child: const Column(
