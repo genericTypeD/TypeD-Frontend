8b830e7c134a242036fa2c0ca79ebb1aa69ad483
H <instantseaside@gmail.com>
2025-02-19 03:36:05 +0900

    feat: 앱 초기화 및 환경 변수 로딩 구현 #17

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,15 @@ import 'package:provider/provider.dart' as provider;
 import 'package:typed/common/provider/app_routes.dart'; // AppRoutes 임포트
 import 'package:typed/common/screen/splash.dart';
 import 'package:typed/sentence/provider/sentence_provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  // Flutter 바인딩 초기화 추가
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // .env 파일 로드
+  await dotenv.load(fileName: ".env");
+
   runApp(
     riverpod.ProviderScope(
       child: provider.MultiProvider(
