0ce41413fa8e3c5bc64295036b18f513b6b26f9b
H <instantseaside@gmail.com>
2025-02-20 23:06:43 +0900

    feat: 빈값 Alert Dialog 구현 #15

--- lib/type/screen/my_sentence_screen.dart
+++ lib/type/screen/my_sentence_screen.dart
@@ -33,6 +33,61 @@ class _MySentenceScreenState extends State<MySentenceScreen> {
     }
   }
 
+  void showAlertBlankSentence() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          actions: [
+            const SizedBox(height: 24),
+            Center(
+              child: Text(
+                '빈 값입니다.',
+                style: AppTheme.body2.copyWith(
+                  fontWeight: FontWeight.bold,
+                  height: 1,
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Divider(
+              color: Colors.black,
+              thickness: 0.3,
+              height: 0.3,
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  minimumSize: Size.zero,
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                ),
+                child: Text(
+                  '확인',
+                  style: AppTheme.body2.copyWith(
+                    height: 1,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+          actionsPadding: EdgeInsets.zero,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          shape: Border.all(
+            color: Colors.black,
+            width: 0.3,
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
