ea249f17b58e06f1ef7e2ba1e0d45794188b471f
H <instantseaside@gmail.com>
2025-02-22 23:23:58 +0900

    feat: EmptyRecordDialog에 콜백 기능 추가 #15

--- lib/type/component/empty_record_dialog.dart
+++ lib/type/component/empty_record_dialog.dart
@@ -4,25 +4,40 @@ import 'package:typed/common/const/index.dart';
 class EmptyRecordDialog extends StatelessWidget {
   final String title;
   final String acceptButtonText;
+  final VoidCallback? onAccept;
 
   const EmptyRecordDialog({
     this.title = AppStrings.emptyRecordDialogTitle,
     this.acceptButtonText = AppStrings.acceptButton,
+    this.onAccept,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      actions: [
+      content: _buildDialogContent(context),
+      actionsPadding: EdgeInsets.zero,
+      contentPadding: EdgeInsets.zero,
+      backgroundColor: Colors.white,
+      elevation: 0,
+      shape: AppBorders.all,
+    );
+  }
+
+  Widget _buildDialogContent(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
         const SizedBox(height: AppSpacings.spacing24),
         Center(
           child: Text(
-            '기록이 비었습니다.',
+            title,
             style: AppTheme.body2.copyWith(
               fontWeight: FontWeight.bold,
               height: 1,
             ),
+            textAlign: TextAlign.center,
           ),
         ),
         const SizedBox(height: AppSpacings.spacing24),
@@ -40,22 +55,19 @@ class EmptyRecordDialog extends StatelessWidget {
               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
             ),
             child: Text(
-              '확인',
+              acceptButtonText,
               style: AppTheme.body2.copyWith(
                 height: 1,
               ),
             ),
             onPressed: () {
+              onAccept?.call();
               Navigator.of(context).pop();
             },
           ),
         ),
         const SizedBox(height: AppSpacings.spacing16),
       ],
-      actionsPadding: EdgeInsets.zero,
-      backgroundColor: Colors.white,
-      elevation: 0,
-      shape: AppBorders.all,
     );
   }
 }
