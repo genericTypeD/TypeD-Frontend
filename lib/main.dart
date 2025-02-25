081bdd46d04a68dc99232398fb6df72fd454e3da
Soojin Son <greennesson@gmail.com>
2025-02-25 15:21:24 +0900

    refactor: 문장 provider의 상태 관리 개선

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart' as riverpod;
-import 'package:provider/provider.dart' as provider;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:typed/common/provider/app_routes.dart';
-import 'package:typed/sentence/provider/sentence_provider.dart';
 
 void main() async {
   // Flutter 바인딩 초기화 추가
@@ -13,13 +11,8 @@ void main() async {
   await dotenv.load(fileName: ".env");
 
   runApp(
-    riverpod.ProviderScope(
-      child: provider.MultiProvider(
-        providers: [
-          provider.ChangeNotifierProvider(create: (_) => SentenceProvider()),
-        ],
-        child: const MyApp(),
-      ),
+    ProviderScope(
+      child: const MyApp(),
     ),
   );
 }
@@ -31,8 +24,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      //home: SplashScreen(), // 초기 화면: SplashScreen
-      // onGenerateRoute: AppRoutes.generateRoute,
       routerConfig: AppRoutes.router,
     );
   }
