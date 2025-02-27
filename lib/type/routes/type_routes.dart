9edcef2a213b70b01b42ab219a0cdddfbb3af900
H <instantseaside@gmail.com>
2025-02-27 18:35:01 +0900

    comment: 불필요한 주석 제거

--- lib/type/routes/type_routes.dart
+++ lib/type/routes/type_routes.dart
@@ -1,23 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:typed/type/screen/my_type.dart';
-//
-// class TypeRoutes {
-//   static Route<dynamic> generateRoute(RouteSettings settings) {
-//     switch (settings.name) {
-//       case '/type':
-//         return MaterialPageRoute(builder: (_) => const MyType());
-//       default:
-//         return MaterialPageRoute(
-//           builder: (_) => Scaffold(
-//             body: Center(
-//               child: Text('No route defined for ${settings.name}'),
-//             ),
-//           ),
-//         );
-//     }
-//   }
-// }
-
 import 'package:go_router/go_router.dart';
 import 'package:typed/type/screens/my_type.dart';
 
