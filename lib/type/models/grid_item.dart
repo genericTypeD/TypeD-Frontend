c07c8ca14320de88b7e18638a90ddff494dc456b
H <instantseaside@gmail.com>
2025-02-28 05:44:10 +0900

    refactor: GridItem에 freezed 패키지 적용 #31

--- lib/type/models/grid_item.dart
+++ lib/type/models/grid_item.dart
@@ -1,13 +1,83 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:typed/type/models/grid_item_type.dart';
 import 'package:image_picker/image_picker.dart';
 
-class GridItem {
-  final String? content;
-  final XFile? imageFile;
-  final bool isEmpty;
+part 'grid_item.freezed.dart';
 
-  GridItem({
-    this.content,
-    this.imageFile,
-    String? id,
-  }) : isEmpty = content == null && imageFile == null;
+@freezed
+abstract class GridItem with _$GridItem {
+  const factory GridItem({
+    required String id,
+    required GridItemType type,
+    String? sentenceContent,
+    String? bookTitle,
+    String? bookAuthors,
+    String? bookImagePath,
+    String? trackTitle,
+    String? trackArtists,
+    String? trackImagePath,
+    XFile? imageFile,
+  }) = _GridItem;
+
+  const GridItem._();
+
+  bool get isEmpty => type == GridItemType.empty;
+
+  /// 빈 그리드 아이템 팩토리 생성자
+  factory GridItem.empty({required String id}) => GridItem(
+        id: id,
+        type: GridItemType.empty,
+      );
+
+  /// 문장 그리드 아이템 팩토리 생성자
+  factory GridItem.sentence({
+    required String id,
+    required String content,
+  }) =>
+      GridItem(
+        id: id,
+        type: GridItemType.sentence,
+        sentenceContent: content,
+      );
+
+  /// 책 그리드 아이템 팩토리 생성자
+  factory GridItem.book({
+    required String id,
+    required String title,
+    String? authors,
+    String? imagePath,
+  }) =>
+      GridItem(
+        id: id,
+        type: GridItemType.book,
+        bookTitle: title,
+        bookAuthors: authors,
+        bookImagePath: imagePath,
+      );
+
+  /// 음악 그리드 아이템 팩토리 생성자
+  factory GridItem.music({
+    required String id,
+    required String title,
+    String? artists,
+    String? imagePath,
+  }) =>
+      GridItem(
+        id: id,
+        type: GridItemType.music,
+        trackTitle: title,
+        trackArtists: artists,
+        trackImagePath: imagePath,
+      );
+
+  /// 이미지 그리드 아이템 팩토리 생성자
+  factory GridItem.image({
+    required String id,
+    required XFile imageFile,
+  }) =>
+      GridItem(
+        id: id,
+        type: GridItemType.image,
+        imageFile: imageFile,
+      );
 }
