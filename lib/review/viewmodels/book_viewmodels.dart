a97d3aee21d5ab2240511f2e83b260bead84b898
H <instantseaside@gmail.com>
2025-03-04 02:41:48 +0900

    feat: 책 검색 관련 프로바이더 추가 #45

---
+++ lib/review/viewmodels/book_viewmodels.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:typed/config/env.dart';
+import 'package:typed/review/book_respository.dart';
+import 'package:typed/review/model/book_model.dart';
+
+/// 책 검색 API를 위한 Repository Provider
+// TODO: - dispose
+final bookRepositoryProvider = Provider<BookRepository>((ref) {
+  final kakaoApiKey = Env.kakaoRestApiKey;
+  return BookRepository(kakaoApiKey: kakaoApiKey);
+});
+
+/// 책 검색 쿼리용 Provider
+final searchQueryProvider = StateProvider<String>((ref) => '');
+
+/// 책 검색 결과용 Provider
+final bookSearchProvider =
+    FutureProvider.family<List<Book>, String>((ref, query) async {
+  if (query.isEmpty) return [];
+
+  final repository = ref.read(bookRepositoryProvider);
+  return repository.searchBooks(query);
+});
+
+/// 선택된 책 Provider
+final selectedBookProvider = StateProvider<Book?>((ref) => null);
