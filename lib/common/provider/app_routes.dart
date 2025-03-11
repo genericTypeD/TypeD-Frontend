46d0e819017efb4506c4d2cc08b8b1862e868f4d
H <instantseaside@gmail.com>
2025-03-11 22:44:30 +0900

    feat: 라우트 이름 추가 #50

--- lib/common/provider/app_routes.dart
+++ lib/common/provider/app_routes.dart
@@ -1,16 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:typed/common/screen/home_tab.dart';
 import 'package:typed/common/screen/splash.dart';
 import 'package:typed/feed/provider/feed_routes.dart';
 import 'package:typed/feed/screen/feed_public.dart';
-import 'package:typed/menu/provider/menu_routes.dart';
-import 'package:typed/menu/screen/my_menu.dart';
 import 'package:typed/review/screen/book_search_screen.dart';
 import 'package:typed/review/provider/review_routes.dart';
+import 'package:typed/review/screen/review_input_screen.dart';
+import 'package:typed/review/screen/review_list_screen.dart';
 import 'package:typed/sentence/provider/sentence_routes.dart';
-import 'package:typed/sentence/screen/sentence_empty.dart';
-import 'package:typed/sentence/screen/sentence_input.dart';
+import 'package:typed/sentence/screen/sentence_list.dart';
 import 'package:typed/type/routes/type_routes.dart';
 import 'package:typed/type/views/my_type.dart';
 
@@ -22,8 +20,6 @@ class AppRoutes {
         path: '/splash',
         builder: (context, state) => const SplashScreen(),
       ),
-
-      // StatefulShellRoute 적용
       StatefulShellRoute.indexedStack(
         builder: (context, state, navigationShell) {
           return HomeTab(navigationShell: navigationShell);
@@ -32,27 +28,40 @@ class AppRoutes {
           StatefulShellBranch(routes: [
             GoRoute(
               path: '/home/type',
+              name: 'mytype_tab',
               builder: (context, state) => const MyType(),
             ),
           ]),
           StatefulShellBranch(routes: [
             GoRoute(
               path: '/home/review',
+              name: 'review_tab',
+              builder: (context, state) => const ReviewListScreen(),
+            ),
+          ]),
+          StatefulShellBranch(routes: [
+            GoRoute(
+              path: '/home/book_search',
+              name: 'book_search_tab',
               builder: (context, state) => const BookSearchScreen(),
             ),
           ]),
           StatefulShellBranch(routes: [
             GoRoute(
-              path: '/home/sentence',
-              builder: (context, state) => const SentenceEmpty(),
+              path: '/home/sentence_list',
+              name: 'sentence_tab',
+              builder: (context, state) => const SentenceList(),
             ),
           ]),
           StatefulShellBranch(routes: [
             GoRoute(
               path: '/home/feed',
+              name: 'feed_tab',
               builder: (context, state) => const FeedPublic(),
             ),
           ]),
+        ],
+      ),
           StatefulShellBranch(routes: [
             GoRoute(
               path: '/home/menu',
