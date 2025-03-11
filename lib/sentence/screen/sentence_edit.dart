0571ae9f425b009f21b86a4c5e6d63efb48d79de
H <instantseaside@gmail.com>
2025-03-12 08:33:59 +0900

    feat: ReviewEditScreen UI 구현

--- lib/sentence/screen/sentence_edit.dart
+++ lib/sentence/screen/sentence_edit.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:typed/common/const/app_colors.dart';
-import 'package:typed/common/const/app_themes.dart';
-import 'package:typed/common/layout/default_layout.dart';
+import 'package:typed/common/const/index.dart';
+import 'package:typed/common/index.dart';
+import 'package:typed/review/models/lock_enum.dart';
 import 'package:typed/sentence/provider/sentence_provider.dart';
 
 class SentenceEdit extends ConsumerStatefulWidget {
@@ -23,6 +23,8 @@ class SentenceEdit extends ConsumerStatefulWidget {
 }
 
 class _SentenceEditState extends ConsumerState<SentenceEdit> {
+  static const _textFieldHintText = '문장을 입력하세요...';
+
   late TextEditingController _controller;
   bool _isPrivate = true;
 
@@ -42,6 +44,7 @@ class _SentenceEditState extends ConsumerState<SentenceEdit> {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      backgroundColor: AppColors.backgroundSecondary,
       appBar: CustomAppBar(
         bottomLeftWidget: GestureDetector(
           onTap: () => context.pop(),
@@ -68,6 +71,125 @@ class _SentenceEditState extends ConsumerState<SentenceEdit> {
           child: Text('문장 수정', style: AppTheme.title3),
         ),
       ),
+      child: Row(
+        children: [
+          Container(
+            width: AppBarStyle.borderContainerWidth,
+            decoration: const BoxDecoration(
+              color: AppColors.backgroundSecondary,
+              border: Border(right: AppBarStyle.borderStyle),
+            ),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SafeArea(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(bottom: AppBarStyle.borderStyle),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SafeArea(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: AppColors.backgroundSecondary,
+                  border: Border(bottom: AppBarStyle.borderStyle),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _controller,
+                        maxLines: 8,
+                        style: AppTheme.body2,
+                        textAlign: TextAlign.left,
+                        textAlignVertical: TextAlignVertical.top,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: AppColors.backgroundSecondary,
+                          hintText: _textFieldHintText,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.zero,
+                            borderSide: const BorderSide(
+                              color: AppColors.borderBlack,
+                              width: 0.3,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.zero,
+                            borderSide: const BorderSide(
+                              color: AppColors.borderBlack,
+                              width: 0.3,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.zero,
+                            borderSide: const BorderSide(
+                              color: AppColors.borderBlack,
+                              width: 0.6,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton.icon(
+                          style: TextButton.styleFrom(
+                            overlayColor: Colors.transparent,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPrivate = !_isPrivate;
+                            });
+                          },
+                          icon: Icon(
+                            _isPrivate ? Icons.lock_outline : Icons.lock_open,
+                            size: 20.0,
+                            color: Colors.black,
+                          ),
+                          label: Text(
+                            _isPrivate
+                                ? LockStatus.closed.korName
+                                : LockStatus.open.korName,
+                            style: AppTheme.body2
+                                .copyWith(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: AppBarStyle.borderContainerWidth,
+            decoration: const BoxDecoration(
+              color: AppColors.backgroundSecondary,
+              border: Border(left: AppBarStyle.borderStyle),
+            ),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SafeArea(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(bottom: AppBarStyle.borderStyle),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
