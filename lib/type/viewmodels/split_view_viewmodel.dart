1beddfbb6c6c310aab3572d3268aad3e141dae9e
H <instantseaside@gmail.com>
2025-03-12 12:41:00 +0900

    feat: SplitView 뷰모델과 Hive 통합 #54

--- lib/type/viewmodels/split_view_viewmodel.dart
+++ lib/type/viewmodels/split_view_viewmodel.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:typed/type/models/period_type.dart';
+import 'package:typed/type/models/split_view_data.dart';
 import 'package:typed/type/models/split_view_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,6 +14,48 @@ final splitViewProvider =
 class SplitViewViewModel extends StateNotifier<SplitViewState> {
   SplitViewViewModel() : super(SplitViewState.initial()) {
     _loadFlex();
+  }
+
+  PeriodType _currentPeriodType = PeriodType.weekly; // 현재 선택된 PeriodType
+  DateTime _currentDateTime = DateTime.now(); // 현재 선택된 날짜
+
+  Future<void> loadForPeriodAndDate(
+      PeriodType periodType, DateTime dateTime) async {
+    debugPrint('[로드 시도] periodType: ${periodType.name}, dateTime: $dateTime');
+
+    _currentPeriodType = periodType;
+    _currentDateTime = dateTime;
+
+    await _loadFromHive(); // Hive에서 데이터 로드
+  }
+
+  Future<void> _loadFromHive() async {
+    try {
+      final box = Hive.box<SplitViewData>('split_view'); // SplitView 전용 박스 열고
+
+      final key = SplitViewData.createKey(
+          _currentPeriodType, _currentDateTime); // 로드할 키 생성
+      debugPrint('로드 키 생성: $_currentPeriodType / $_currentDateTime => $key');
+
+      final data = box.get(key); // 박스에서 데이터 로드 후,
+
+      if (data != null) {
+        // 로드된 데이터로 상태 업데이트
+        state = SplitViewState(
+          horizontalFlexValues: data.horizontalFlexValues,
+          verticalFlexValues: data.verticalFlexValues,
+        );
+        debugPrint('[SplitView 데이터 로드 완료] key: $key');
+      } else {
+        // 해당 기간/날짜의 데이터가 없으면 기본값 사용
+        state = SplitViewState.initial();
+        debugPrint('[SplitView 기본 데이터 사용해서 로드 완료]: $key');
+      }
+    } catch (error) {
+      // 오류 발생 시에도 기본값 사용
+      state = SplitViewState.initial();
+      debugPrint('[SplitView 데이터 로드 실패] error: $error');
+    }
   }
 
   Future<void> _loadFlex() async {
