2e55b37d02c35f9e79b60cbf10fa33a5fdd66fe6
H <instantseaside@gmail.com>
2025-03-14 22:39:10 +0900

    refactor: ReviewRepository 에러 처리 방식 통일

--- lib/review/data/repositories/review_repository.dart
+++ lib/review/data/repositories/review_repository.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:typed/review/data/models/review_model.dart';
+import 'package:typed/review/data/repositories/review_exceptions.dart';
 
 enum SortOption {
   id,
@@ -12,11 +14,18 @@ class ReviewRepository {
   final Box<Review> _box = Hive.box<Review>('review');
 
   /// 서평 저장 (POST)
-  // TODO: - 새로 추가된 서평을 반환하는 것으로 에러 처리 개선
-  // TODO: - catch문 내 로깅 함수 추가
-  Future<String> addReview(Review review) async {
-    final key = await _box.add(review);
-    return key.toString();
+  // TODO: - 새로 추가된 서평을 반환하는 것으로 에러 처리 개선 ✅
+  // TODO: - catch문 내 로깅 함수 추가 ✅
+  Future<Review> addReview(Review review) async {
+    try {
+      await _box.add(review);
+      return review;
+    } catch (error) {
+      final errorString = '서평 저장';
+
+      _logError(errorString, error);
+      throw ReviewStorageException(errorString);
+    }
   }
 
   /// 서평 전체 목록 조회 (GET)
@@ -61,51 +70,87 @@ class ReviewRepository {
 
   // TODO: - 검색 키워드가 포함된 bookTitle 혹은 content가 있는 서평 목록 조회(키워드 검색 기능)
   // TODO: - 정렬된 상태로 목록 조회
-
   /// ISBN으로 특정 책의 서평 목록 조회 (GET)
   Future<List<Review>> fetchReviewsByIsbn(String isbn) async {
-    return _box.values.where((review) => review.bookIsbn == isbn).toList();
+    try {
+      return _box.values.where((review) => review.bookIsbn == isbn).toList();
+    } catch (error) {
+      final errorString = 'ISBN으로 특정 책의 서평 목록 조회';
+
+      _logError(errorString, error);
+      throw ReviewStorageException(errorString);
+    }
   }
 
   /// ID로 특정 서평 조회 (GET)
   Review? getReviewById(int id) {
     try {
       return _box.values.firstWhere((review) => review.id == id);
-    } catch (e) {
+    } catch (error) {
+      final errorString = 'ID로 특정 서평 조회';
+
+      _logError(errorString, error);
+      // throw ReviewException('ID로 특정 책의 서평을 불러오는데 실패했습니다: $error');
       return null;
-      // } finally {
-      //   client.close();
-      // }
     }
   }
 
   /// ID로 특정 서평의 index 조회 (GET)
   int getReviewIndex(Review reviewToSearch) {
-    final index = _box.values.toList().indexWhere(
-          (review) => review.id == reviewToSearch.id,
-        );
-    return index;
+    try {
+      final index = _box.values.toList().indexWhere(
+            (review) => review.id == reviewToSearch.id,
+          );
+      return index;
+    } catch (error) {
+      final errorString = 'ID로 특정 서평의 index 조회';
+
+      _logError(errorString, error);
+      throw ReviewStorageException(errorString);
+    }
   }
 
   /// 서평 수정 (PUT)
   Future<void> updateReview(Review reviewToUpdate) async {
-    final index = getReviewIndex(reviewToUpdate); // 해당 ID의 서평을 찾아 인덱스 확인
+    try {
+      final index = getReviewIndex(reviewToUpdate); // 해당 ID의 서평을 찾아 인덱스 확인
 
-    if (index != -1) {
-      await _box.putAt(index, reviewToUpdate); // 해당 인덱스의 서평 업데이트
-    } else {
-      throw Exception('서평을 찾을 수 없습니다. ID: ${reviewToUpdate.id}');
+      if (index != -1) {
+        await _box.putAt(index, reviewToUpdate); // 해당 인덱스의 서평 업데이트
+      } else {
+        throw ReviewNotFoundException(reviewToUpdate.id);
+      }
+    } catch (error) {
+      final errorString = '서평 수정';
+      _logError(errorString, error);
+
+      if (error is ReviewNotFoundException) {
+        rethrow;
+      } else {
+        throw ReviewStorageException(errorString);
+      }
     }
   }
 
   /// 서평 삭제 (DELETE)
   Future<void> deleteReview(Review reviewIdToDelete) async {
-    final index = getReviewIndex(reviewIdToDelete); // 해당 ID의 서평을 찾아 인덱스 확인
+    try {
+      final index = getReviewIndex(reviewIdToDelete); // 해당 ID의 서평을 찾아 인덱스 확인
 
-    if (index != -1) {
-      await _box.deleteAt(index); // 해당 인덱스의 서평 삭제
-    } else {
-      throw Exception('서평을 찾을 수 없습니다. ID: $reviewIdToDelete.id');
+      if (index != -1) {
+        await _box.deleteAt(index); // 해당 인덱스의 서평 삭제
+      } else {
+        throw ReviewNotFoundException(reviewIdToDelete.id);
+      }
+    } catch (error) {
+      final errorString = '서평 삭제';
+      _logError(errorString, error);
+
+      if (error is ReviewNotFoundException) {
+        rethrow;
+      } else {
+        throw ReviewStorageException(errorString);
+      }
     }
   }
 
